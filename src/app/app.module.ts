1126365b7db4a2eef6e30bde0ee310080ae96ea5
RichardMarkRoss <cptrichkidprice@gmail.com>
2022-06-14 13:01:40 +0200

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GalleryComponent
   ],
   imports: [
     BrowserModule,
